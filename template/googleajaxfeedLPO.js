dffb7bc411b2ec99cd5832aaa91a2db475fe6d40
coolniikou <cool.ni.ikou@gmail.com>
2009-02-17 01:38:08 +0900

    first commit

---
+++ template/googleajaxfeedLPO.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+      
+      google.load("feeds", "1");
+
+      function initialize() {
+	var pattern = /[&\?](p|q|query|MT|qt|searchText|kw)=([^&]+)/;
+	var ref = document.referrer.match(pattern);
+	var refword = decodeURI(ref[2]);
+       var feedControl = new google.feeds.FeedControl();
+        feedControl.addFeed("http:/[path/to/mt]/mt-search.cgi?search="+refword+"&Template=feed&IncludeBlogs=1&", "refword");
+        feedControl.draw(document.getElementById("feedControl"));
+      }
+      google.setOnLoadCallback(initialize);
+
+</script>
