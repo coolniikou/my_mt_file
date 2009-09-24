dfd75cdf2c1a09c1a012972267562de332b138ed
root <root@blog-design.org>
2009-09-24 12:18:17 +0900

    first commit

---
+++ plugin/JSONAccessRanking/access_ranking_json.js
@@ -0,0 +1,43 @@
+// jQuery AccessRanking show list from json Google analytics data.
+
+$(document).ready(function(){
+	var uri = 'http://domain/path/to/accessranking.json';
+	var base_uri = 'http://domain (no slash)';
+	var	list = '<h4>Weekly アクセスランキング</h4>';
+		list += '<ul class="rk_list">';
+	
+	$.getJSON(uri, function(data){
+		var a = data;
+		var c = 1;
+		for (i=0; i<a.length; i++) {
+			var title = a[i]['dxp:dimension']['ga:pageTitle'].value;
+			    title = title.substr(0,40);
+				title += '...';
+			var url = a[i]['dxp:dimension']['ga:pagePath'].value;
+			list += '<li class="rk_item">'+c+' : '+'<a href="'+url+'">'+title+'</a></li>';
+			c++;
+		}
+		list += '</ul>';
+		$("div.accessranking").empty().html(list);
+	});
+	return false;
+});
+
+/*--
+HTML
+<div class="accessranking"></div>
+
+
+CSS
+ul.rk_list {
+	list-style-image: none;
+	list-style-position: outside;
+	list-style-type: none; }
+
+li.rk_item {
+	margin: 0;
+	padding: 0; }
+
+
+
+--*/
