be387d005975d502d74f9cb4dcf5a26298988f77
root <root@blog-design.org>
2009-09-24 11:01:32 +0900

    first commit

---
+++ plugin/JSONAccessRanking/lib/JSONAccessRanking/L10N/en_us.pm
@@ -0,0 +1,14 @@
+package JSONAccessRanking::L10N::en_us;
+
+use strict;
+use base 'JSONAccessRanking::L10N';
+use vars qw( %Lexicon );
+%Lexicon = (
+	'as_NAME' => 'JSONAccessRanking',
+	'as_AUTHOR_NAME' => 'cool_ni_ikou',
+	'as_DESCRIPTION' => 'JSONAccessRanking makes it easy to publish JSON Data of Access Ranking from Google Analytics.',
+	'as_AssetExif' => 'JSONAccessRanking',
+	'as_SET_DES' => 'require initial setup your Google analytics login data. ',
+);
+
+1;
