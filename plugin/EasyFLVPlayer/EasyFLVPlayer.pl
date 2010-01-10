667cce6200cc46732379abac9fb9ba5d59d4234d
coolniikou <cool.ni.ikou@gmail.com>
2010-01-10 18:14:29 +0900

    touch up

--- plugin/EasyFLVPlayer/EasyFLVPlayer.pl
+++ plugin/EasyFLVPlayer/EasyFLVPlayer.pl
@@ -25,6 +25,7 @@ my $plugin; $plugin =  MT::Plugin::EasyFLVPlayer->new({
     },
 });
 MT->add_plugin($plugin);
+
 sub doLog {
     my ($msg) = @_; 
     return unless defined($msg);
@@ -44,53 +45,53 @@ sub _asset_insert {
     my $asset_id = $1 if ( $upload_html =~ /<form.*?mt:asset\-id="([0-9]{1,})".*?>/ );
     if ( $asset_id ) {
     	require MT::Asset;
-   		my $asset = MT::Asset->load( $asset_id );
+  	my $asset = MT::Asset->load( $asset_id );
     	my $asset_class = $asset->{column_values}->{class};
     	my $asset_ext = $asset->{column_values}->{file_ext};
     	if ( $asset_class eq 'video' && $asset_ext eq 'flv' ) {
     		my $asset_label = $asset->{column_values}->{label};
     		(my $asset_url = $asset->{column_values}->{url} ) =~ s/%r\///;
-    			$asset_url = $site_url.$asset_url;
+    		    $asset_url = $site_url.$asset_url;
     		my $text = '';
-    			$text = sprintf(
-    						'<div class="flv_movie">
-    							<div class="flv_player">
-    							<form mt:asset-id="%s" class="mt-enclosure mt-enclosure-video" style="display: inline;">
-    							<object id="player" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" name="player" width="400" height="315">
-    								<param name="movie" value="%s/player-viral.swf" />
-									<param name="allowfullscreen" value="true" />
-									<param name="allowscriptaccess" value="always" />
-									<param name="flashvars" value="file=%s&image=%s/preview.jpg" />
-									<embed
-										type="application/x-shockwave-flash"
-										id="player2"
-										name="player2"
-										src="%s/player-viral.swf" 
-										width="400" 
-										height="315"
-										allowscriptaccess="always" 
-										allowfullscreen="true"
-										flashvars="file=%s&image=%s/preview.jpg" 
-									/>
-								</object>
-								</form>
-								</div>
-								<div class="flv_link>
-									<a href="%s" class="link_flv" title="%s">%s</a>
-								</div>
-							</div>',
-								$asset_id,
-								$static_path,
-								$asset_url,
-								$static_path,
-								$static_path,
-								$asset_url,
-								$static_path,
-								$asset_url,
-								$asset_label,
-								$asset_label,
-							);
-			$upload_html = $text;					
+    		   $text = sprintf(
+    			'<div class="flv_movie">
+    				<div class="flv_player">
+    				<form mt:asset-id="%s" class="mt-enclosure mt-enclosure-video" style="display: inline;">
+    				<object id="player" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" name="player" width="400" height="315">
+    					<param name="movie" value="%s/player-viral.swf" />
+					<param name="allowfullscreen" value="true" />
+					<param name="allowscriptaccess" value="always" />
+					<param name="flashvars" value="file=%s&image=%s/preview.jpg" />
+					<embed 
+						type="application/x-shockwave-flash"
+						id="player2" 
+						name="player2" 
+						src="%s/player-viral.swf" 
+						width="400" 
+						height="315" 
+						allowscriptaccess="always" 
+						allowfullscreen="true" 
+						flashvars="file=%s&image=%s/preview.jpg" 
+					/>
+				</object>
+				</form>
+				</div>
+				<div class="flv_link>
+					<a href="%s" class="link_flv" title="%s">%s</a>
+				</div>
+			</div>',
+				$asset_id,
+				$static_path,
+				$asset_url,
+				$static_path,
+				$static_path,
+				$asset_url,
+				$static_path,
+				$asset_url,
+				$asset_label,
+				$asset_label,
+			);
+		$upload_html = $text;					
     	}
     }
     $param->{ upload_html } = $upload_html;
